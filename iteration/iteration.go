515942dd26b023cc4ae3f84763f774815c73cf71
Ashdevelopernewbie <aahashish100@gmail.com>
2024-06-12 17:09:33 +0545

    feat: added example and new feat of accepting word as well as the number

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,7 +1,6 @@
 package iteration
 
-func Iteration(count int) string {
-	word := "p"
+func Iteration(count int, word string) string {
 	var newWord string
 	for i := 0; i < count; i++ {
 		newWord += word
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,12 +1,15 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIteration(t *testing.T) {
 	//Let's write a test for a function that repeats a character 5 times.
 	// Comment #2 I did it the opposiste instead of repeating any character 5 times. I repeated the same word 5 times statically. Now change that
-	got := Iteration(5)
-	expected := "ppppp"
+	got := Iteration(5, "comment")
+	expected := "commentcommentcommentcommentcomment"
 
 	if got != expected {
 		t.Errorf("\nexpected 	%v \ngot 		%v", expected, got)
@@ -17,6 +20,12 @@ func TestIteration(t *testing.T) {
 
 func BenchmarkIteration(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Iteration(5)
+		Iteration(5, "comment")
 	}
+}
+
+func ExampleIteration() {
+	got:= Iteration(10, "a")
+	fmt.Println(got)
+	// Output: aaaaaaaaaa
 }
